30f4ad594f53e4829a4aaede3001fabd63c009df
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2024-08-13 10:04:42 -0300

    Criação do jogo em desenvolvimento do campo minado

--- Aula-Python-12-08/main.py
+++ Aula-Python-12-08/main.py
@@ -1,158 +1,104 @@
-'''
-Criação de um jogo de Campo Minado
-
-01 - Atividade 
-- O sistema deve solicitar um número ao usuario
-- se o numero for menor ou igual a 0 peça que o usuario digite novamente até receber um numero valido
-- o sistema deve percorrer do 0 até o numero escolhido
-- no laço se o numero for PAR deve focar em Azul e se for IMPAR deve ficar Vermelho
-
-numero = input('Digite um Número Maior Que 0: ');
-while int(numero) <= 0:
-    numero = input('\nDigite Um Novo Número: ');
-if int(numero) <= 0:
-    numero = input('\nDigite Um Novo Número: ');
-    print('\n');
-elif int(numero) != 0:
-   print('\n');
-for i in range(0, int(numero) + 1):
-    if i % 2 == 0:
-        print('\033[31m' + str(i) + '\033[31m');
-    else:
-        print('\033[34m' + str(i) + '\033[34m');
-print('\n');
-'''
-
-'''
-02 - Solicitar ao usuario um digito
-deve apresentar a quantidade em Asteristico (*) na tela
-solicitar 2 número, deve ser a quantidade de linhas apresentada em tela
-'''
-'''
-var = ''
-quantidade_Coluna = int(input("Digite a quantidade de Coluna(s) por linha(s): "));
-quantidade_Linhas = int(input("Digite a quantidade de Linhas: "));
-
-
-for i in range(int(quantidade_Linhas)):
-    for var in range(0, int(quantidade_Coluna)):
-        print('*' * quantidade_Coluna);
-    print()    
-'''
-'''
-
-
-quantColuna = 0;
-quantLinhas = 0;
-
-quantColuna = int(input("Digite a quantidade de Coluna(s) por linha(s): "))
-quantLinhas = int(input("Digite a quantidade de Linhas: "))
-
-for i in range(int(quantLinhas)):
-    for j in range(0, int(quantColuna)):
-        print('{:1}' .format('*'), end=''); #{} é a quantidade de espaçamento que ficará entre cada caracteres, e o format é o formato que o caracteres terá
-    print()
-'''
-
 from openpyxl import Workbook, load_workbook
 
-def config():
+def configs():  
     try:
-        wb = load_workbook(filename='Campo.xlsx')
-        config = wb['config']
+       wb = load_workbook(filename = 'campo.xlsx')
+       config = wb['config']
     except:
-        wb= Workbook()
+        wb = Workbook()
+
         config = wb.create_sheet('config')
-        config.cell(column=1, row=1, value="linha")
-        config.cell(column=2, row=1, value= 5)
-        config.cell(column=1, row=2, value="coluna")
-        config.cell(column=2, row=2, value= 5)
+        config.cell(column = 1, row = 1, value = 'linhas')
+        config.cell(column = 2, row = 1, value = 5)
+        config.cell(column = 1, row = 2, value = 'colunas')
+        config.cell(column = 2, row = 2, value = 5)
+        config.cell(column = 1, row = 3, value = 'dificuldade')
+        config.cell(column = 2, row = 3, value = 2)
 
-    linhas = config.cell(column=2, row=1).value
-    colunas = config.cell(column=2, row=2).value
+        linhas = config.cell(column = 2, row = 1).value
+        colunas = config.cell(column = 2, row = 2).value
+        dificuldade = config.cell(column = 2, row = 3).value
 
-    wb.save('Campo.xlsx')
-    return linhas, colunas
+        wb.save('campo.xlsx')
+     
+        config = {
+        
+            'linhas' : int(linhas),
+            'colunas' : int(colunas),
+            'dificuldade' : int(dificuldade)
+        }
+     
+        return config
 
-def criarTabuleiro(quantColunas, quantLinhas):
-    for i in range(int(quantLinhas)):
-        for j in range(0, int(quantColunas)):
-            print('{:5}' .format('[ ]'), end='');
-        print() 
-
+def CriarTabuleiro(quantLinhas,quantColunas):
     
-# quantColuna = int(input("Digite a quantidade de Coluna(s) por linha(s): "))
-# quantLinhas = int(input("Digite a quantidade de Linhas: "))
+    for i in range(0, int(quantLinhas)):
+      for j in range(0, int(quantColunas)):
+         print('{:5}'.format(' [ ] '), end = '')
+      print()
 
-quantLinhas, quantColunas = config();
-criarTabuleiro(quantColunas, quantLinhas);
+def ConfigCampo():
 
-'''
-Crie o menu de acesso do jogo com as seguintes opções "jogar, configurar, sair"
-Hoje a gente vai criar o configurar, de as opções ao usuario para ele configurar o jogo dele
-Nessas configurações coloque uma terceira config "dificuldade" o usuario vai pode escolher entre 1 - 3
-'''
+   NovaQuantColunas = input('\n Quantas colunas: ')
+   NovaQuantLinhas = input(' Quantas linhas: ')
+   NovaDificuldade = input('\n 1- Facil \n 2- Medio \n 3- Dificil \n\n Escolha a dificuldade: ')
 
+   try:
+
+      wb = load_workbook(filename = 'campo.xlsx')
+      config = wb['config']
+
+   except:
+
+      wb = Workbook()
+      config = wb.create_sheet('config')
+      
+   config.cell(column = 1, row = 1, value = 'linhas')
+   config.cell(column = 2, row = 1, value = NovaQuantLinhas)
+   config.cell(column = 1, row = 2, value = 'colunas')
+   config.cell(column = 2, row = 2, value = NovaQuantColunas)
+   config.cell(column = 1, row = 3, value = 'dificuldade')
+   config.cell(column = 2, row = 3, value = NovaDificuldade)
+
+
+   wb.save('campo.xlsx')
+
+def CalcularBomba():  
+    config = configs()
+    totalCasas = config['linhas'] * config['colunas']
+
+    if config['dificuldade'] == 1:   
+      quantBomba = totalCasas * 0.15
+      
+    elif config['dificuldade'] == 2:     
+      quantBomba = totalCasas * 0.3
+      
+    elif config['dificuldade'] == 3:   
+      quantBomba = totalCasas * 0.5
+
+    print('Total de bombas: ' .format(quantBomba))
+      
+config = configs()
+CriarTabuleiro(config['linhas'], config['colunas'])
+CalcularBomba()
 
 while True:
-    opcao = int(input("Escolha uma opção do Menu: "))
 
-    if opcao == '1':
-        print("Iniciando o jogo...")
-    elif opcao == '2':
-        print("Configurar o jogo..")
-    elif opcao == '3':
-        print("Saindo do jogo...")
-    else:
-        print( '\033[31m'"Opção inválida!"'\033[0;0m')
+   opcao = input ('\n 1- jogar \n 2- Configurar \n 3- Sair \n\n Escolha: ')
 
+   if opcao == '1':
+      print('\n jogar')
 
+   elif opcao == '2':
+      print('\n Configurar')
+      ConfigCampo()
 
-def menu_principal():
-    print("Menu Principal")
-    print("1. Jogar")
-    print("2. Configurar")
-    print("3. Sair")
+      config = configs()
+      CriarTabuleiro(config['linhas'], config['colunas'])
 
-    opcao = int(input("Escolha uma opção do Menu: "))
+   elif opcao == '3':
+      print('\n Sair')
+      break
 
-    if opcao == 1:
-        print("Iniciando o jogo...")
-    elif opcao == 2:
-        menu_configuracao()
-    elif opcao == 3:
-        print("Saindo do jogo...")
-    else:
-        print("Opção inválida!")
-        menu_principal()
-        
-def menu_configuracao():
-    print("Você Entrou Nas Configurações do Jogo!")
-    print("1. Dificuldade")
-    print("2. Voltar ao Menu Principal")
-    
-    opcao = int(input("Escolha A dificuldade: "))
-
-    if opcao == 1:
-        configurar_dificuldade()
-    elif opcao == 2:
-        menu_principal()
-    else:
-        print("Opção inválida!")
-        menu_configuracao()
-
-def configurar_dificuldade():
-    print("\nConfigurar Dificuldade")
-    print("1. Fácil")
-    print("2. Médio")
-    print("3. Difícil")
-
-    dificuldade = int(input("Escolha a dificuldade: "))
-    
-    if dificuldade in [1, 2, 3]:
-        print(f"Dificuldade configurada para {dificuldade}")
-    else:
-        print("Opção inválida!")
-        configurar_dificuldade()
-
-menu_principal()
+   else:
+      print('Erro!!')
